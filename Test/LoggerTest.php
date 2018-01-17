f4b8798f8016e0cc37dca052f4abb95baf2605c6
Simon Hampel <simon@hampelgroup.com>
2018-01-17 14:15:49 +1100

    simplify logger test

--- Test/LoggerTest.php
+++ Test/LoggerTest.php
@@ -11,30 +11,15 @@ class LoggerTest extends AbstractTest
 
 		$context = ['a' => 'foo', 'b' => 'bar', 'c' => 'baz'];
 
-		$result[0] = $logger->debug('this is a debug message', $context);
-		$result[1] = $logger->info('this is an info message');
-		$result[2] = $logger->notice('this is a notice message');
-		$result[3] = $logger->warning('this is a warning message');
-		$result[4] = $logger->error('this is an error message');
-		$result[5] = $logger->critical('this is a critical message');
-		$result[6] = $logger->alert('this is an alert message');
-		$result[7] = $logger->emergency('this is an emergency message');
+		$logger->debug('this is a debug message', $context);
+		$logger->info('this is an info message');
+		$logger->notice('this is a notice message');
+		$logger->warning('this is a warning message');
+		$logger->error('this is an error message');
+		$logger->critical('this is a critical message');
+		$logger->alert('this is an alert message');
+		$logger->emergency('this is an emergency message');
 
-		$success = true;
-		foreach ($result as $r)
-		{
-			if (!$r) $success = false;
-		}
-
-		if ($success)
-		{
-			$this->successMessage(\XF::phrase('monolog_log_successfully_processed'));
-			return true;
-		}
-		else
-		{
-			$this->errorMessage(\XF::phrase('monolog_log_could_not_be_processed'));
-			return false;
-		}
+		return true;
 	}
 }
