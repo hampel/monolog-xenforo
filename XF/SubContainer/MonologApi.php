38dd460987e998bef22500fcbfc033647ddb3bbc
Simon Hampel <simon@hampelgroup.com>
2018-01-17 17:47:43 +1100

    added email deduplication timeout option

--- XF/SubContainer/MonologApi.php
+++ XF/SubContainer/MonologApi.php
@@ -1,5 +1,6 @@
 <?php namespace Monolog\XF\SubContainer;
 
+use Monolog\Option\EmailDeduplicationTimeout;
 use Monolog\Option\EmailMinimumLogLevel;
 use Monolog\Option\EmailSubject;
 use Monolog\Option\SendEmail;
@@ -44,15 +45,14 @@ class MonologApi extends AbstractSubContainer
 			$subject = EmailSubject::get();
 			$sendTo = SendEmail::getAddress();
 			$logLevel = EmailMinimumLogLevel::get();
+			$dedupTimeout = EmailDeduplicationTimeout::get();
 
 			$message = $this->getSwiftMessage($subject, $sendTo);
 			$swiftmailer = \Swift_Mailer::newInstance($this->app->mailer()->getDefaultTransport());
 
-			// TODO: add minimum log level option for emails
-			// TODO: add log level dedup timeout for emails
 			$handler = new SwiftMailerHandler($swiftmailer, $message, $logLevel);
 
-			return new DeduplicationHandler($handler, $tempDir.'/monolog-dedup-swiftmailer.log', $logLevel, 300);
+			return new DeduplicationHandler($handler, $tempDir.'/monolog-dedup-swiftmailer.log', $logLevel, $dedupTimeout);
 		};
 
 		$container['processor.visitor'] = function ($c)
