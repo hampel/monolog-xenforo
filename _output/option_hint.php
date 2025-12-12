7490e806af85f6aad1c6087e30a93a7da5eb1b48
Simon Hampel <simon@hampelgroup.com>
2025-12-12 16:03:30 +1100

    latest composer dependencies

---
+++ _output/option_hint.php
@@ -0,0 +1,25 @@
+<?php
+
+// ################## THIS IS A GENERATED FILE ##################
+// DO NOT EDIT DIRECTLY. EDIT THE OPTIONS IN THE CONTROL PANEL.
+
+/**
+ * @noinspection PhpMultipleClassDeclarationsInspection
+ * @noinspection PhpIllegalPsrClassPathInspection
+ */
+
+namespace XF;
+
+/**
+ * @property bool|null $monologAddVisitorExtra Add Visitor Extra Data
+ * @property bool|null $monologAddWebExtra Add Web Extra Data
+ * @property non-negative-int|null $monologEmailDeduplicationTimeout Email Deduplication Timeout
+ * @property non-negative-int|null $monologEmailMinimumLogLevel Email Minimum Log Level
+ * @property string|null $monologEmailSubject Email Subject
+ * @property non-negative-int|null $monologFileMinimumLogLevel Log File Minimum Log Level
+ * @property array{enabled: string, logfile: string}|null $monologLogFile Send Logs to File
+ * @property array{enabled: string, email: string}|null $monologSendEmail Send Logs via Email
+ */
+class Options
+{
+}
